d93262f5996a6953382c61fafcb0f4aad2fe4b38
BFerratto <bruferratto@gmail.com>
2017-06-15 19:36:35 -0300

    Restarted timer functions

--- index.js
+++ index.js
@@ -1,24 +1,59 @@
-var pomodoro;
-window.onload = function(){
-  pomodoro.init();
-};
-var pomodoro ={
-  started: false,
-  minutes:0,
-  seconds:0,
-  interval: null,
-  minutesDOM:null,
-  secpondsDOM:null,
+intervalId = null;
+var initialTime = null;
+var pausedTime = 0;
 
-  init: function(){
-    var event = this;
-    this.minutesDOM = document.querySelector('#minutes');
-    this.secondsDOM = document.querySelector('#seconds');
-    this.interval = setInterval(function(){
-      event.intervalCallback.apply(event);
-    }, 1000);
-    document.querySelector('#start').onclick = function () {
-      event.startWork.apply(event);
-    };
-    }
-  }
+function startTimer (){
+  var time = document.getElementById('Work-length').innerHTML;
+  initialTime = new Date().getTime();
+  intervalId = window.setInterval(countdown,1000,time)
+}
+function pauseTimer(){
+  window.clearInterval(intervalId);
+  var newTime = new Date().getTime();
+  pausedTime = newTime - initialTime;
+}
+function stopTimer (){
+  window.clearInterval(intervalId);
+  var actualLength = document.getElementById('Work-length').innerHTML
+  document.getElementById('minutes').innerHTML = actualLength;
+  document.getElementById('seconds').innerHTML = "00";
+}
+function countdown (totalTime){
+  var currentTime = new Date().getTime();
+  var ellapsedTime = currentTime - initialTime;
+  ellapsedTime = Math.floor(ellapsedTime/1000);
+  var totalSec = totalTime*60;
+  var remainingSec = totalSec - ellapsedTime;
+  var displayMin = Math.floor(remainingSec/60);
+  var displaySec = remainingSec%60;
+  document.getElementById('minutes').innerHTML = displayMin;
+  document.getElementById('seconds').innerHTML = displaySec;
+
+  console.log(displaySec)
+  console.log(displayMin);
+}
+
+function decreaseWork(){
+  var totalTime = document.getElementById('Work-length').innerHTML;
+  var decreased = Math.max (totalTime-1, 0 );
+  document.getElementById('Work-length').innerHTML = decreased;
+  document.getElementById('minutes').innerHTML = decreased;
+}
+function increaseWork(){
+  var totalTime = document.getElementById('Work-length').innerHTML;
+  var increase = Math.min(~~totalTime+1, 30);
+  document.getElementById('Work-length').innerHTML = increase;
+  document.getElementById('minutes').innerHTML = increase;
+}
+function decreaseBreak(){
+  var totalTime = document.getElementById('break-length').innerHTML;
+  var decreased = Math.max (totalTime-1, 0 );
+  document.getElementById('break-length').innerHTML = decreased;
+  document.getElementById('minutes').innerHTML = decreased;
+}
+function increaseBreak(){
+  var totalTime = document.getElementById('break-length').innerHTML;
+  var increase = Math.min(~~totalTime+1, 15);
+  document.getElementById('break-length').innerHTML = increase;
+  document.getElementById('minutes').innerHTML = increase;
+}
